93b49366c490096b5fdad5b18efb9ed7c9f16485
Muhammad Mustaqim Chowdhury <102687649+cnkqraw@users.noreply.github.com>
2026-09-01 18:18:45 +0300

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -3,7 +3,7 @@ export async function onRequest(context) {
   const url = new URL(request.url);
 
   const isSocialPreview =
-    url.pathname === "/assets/social-preview.png";
+    url.pathname === "/assets/social-preview.jpg";
   
   const isProtectedImage =
     url.pathname.startsWith("/assets/") &&
